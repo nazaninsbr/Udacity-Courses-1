38bcd957f501770496acceff04387867486f6ef4
Nazanin Sabri <nazanin.sabrii@gmail.com>
2017-10-02 18:53:55 +0330

    a new course and some work on JS

---
+++ Introduction to JavaScript/data types and variables/concat.js
@@ -0,0 +1,18 @@
+/*
+ * Programming Quiz: One Awesome Message (2-12)
+ *
+ * 1. Create the variables
+ *     - firstName
+ *     - interest
+ *     - hobby
+ * 2. Create a variable named awesomeMessage and, using string concatenation and the variables above, create an awesome message.
+ * 3. Print the awesomeMessage variable to the console
+ */
+ 
+ // Add your code here
+
+var firstName="Nazanin";
+var interest="movies";
+var hobby="programming";
+var awesomeMessage="Hi, my name is "+firstName+". I love "+interest+". In my spare time, I like to "+hobby+".";
+console.log(awesomeMessage);
---
+++ Introduction to JavaScript/data types and variables/semiColon.js
@@ -0,0 +1,8 @@
+/*
+ * Programming Quiz: Semicolons! (2-8)
+ */
+
+// your code goes here
+thingOne = "red";
+thingTwo = " blue";
+console.log(thingOne+thingTwo);
