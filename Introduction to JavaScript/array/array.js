9c884b151b1eed15b28920245d3a2d4cd8fecbdb
Nazanin Sabri <nazanin.sabrii@gmail.com>
2017-10-06 15:06:59 +0330

    add intro to js until the end of lesson 5

---
+++ Introduction to JavaScript/array/array.js
@@ -0,0 +1,7 @@
+/*
+ * Programming Quiz: UdaciFamily (6-1)
+ */
+
+// your code goes here
+udaciFamily = ["Julia", "James", "Nazanin"];
+console.log(udaciFamily);
---
+++ Introduction to JavaScript/array/splice.js
@@ -0,0 +1,8 @@
+/*
+ * Programming Quiz: Colors of the Rainbow (6-4)
+ */
+
+var rainbow = ["Red", "Orange", "Blackberry", "Blue"];
+rainbow.splice(2, 1);
+rainbow.splice(3, 0,"Yellow", "Green", "Purple");
+console.log(rainbow);
